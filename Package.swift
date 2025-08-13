bc7194f4b8ba5643cd9c4dcef24205029612dfa4
Ahmed Raad <araad@creativeadvtech.com>
2025-08-14 00:51:59 +0300

    first commit

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BabylAI-iOS",
+    platforms: [.iOS(.v16)],
+    products: [
+
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ably/ably-cocoa", from: "1.2.41"),
+        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.2")
+    ],
+    targets: [
+        .binaryTarget(name: "BabylAI", path: "./Sources/BabylAI.xcframework")
+    ]
+)
---
+++ Sources/BabylAI-iOS/BabylAI.xcframework/ios-arm64/BabylAI.framework/Headers/BabylAI.h
@@ -0,0 +1,19 @@
+//
+//  BabylAI.h
+//  BabylAI
+//
+//  Created by Ahmed Raad on 05/08/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for BabylAI.
+FOUNDATION_EXPORT double BabylAIVersionNumber;
+
+//! Project version string for BabylAI.
+FOUNDATION_EXPORT const unsigned char BabylAIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BabylAI/PublicHeader.h>
+
+
