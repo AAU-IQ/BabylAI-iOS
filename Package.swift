2d3fbc3889ae55ce9477a72a8296247dd609e6c4
Ahmed Raad <araad@creativeadvtech.com>
2025-08-14 00:58:42 +0300

    Fix package.swift binary path

--- Package.swift
+++ Package.swift
@@ -7,13 +7,10 @@ let package = Package(
     name: "BabylAI-iOS",
     platforms: [.iOS(.v16)],
     products: [
-
+        .library(name: "BabylAI", targets: ["BabylAI"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/ably/ably-cocoa", from: "1.2.41"),
-        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.2")
-    ],
+    dependencies: [],
     targets: [
-        .binaryTarget(name: "BabylAI", path: "./Sources/BabylAI.xcframework")
+        .binaryTarget(name: "BabylAI", path: "Sources/BabylAI-iOS/BabylAI.xcframework")
     ]
 )
