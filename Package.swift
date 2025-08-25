1923c1d6b5beaa71aa8bfc852eaa01f79533cd85
Ahmed Raad <araad@creativeadvtech.com>
2025-08-26 00:43:22 +0300

    Support iOS 13+

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BabylAI-iOS",
-    platforms: [.iOS(.v16)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "BabylAI", targets: ["BabylAI"])
     ],
